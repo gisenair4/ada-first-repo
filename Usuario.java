7f4a2fe9d63eb324932f6c53665556e4cd75bdfb
gisenair4 <63479061+gisenair4@users.noreply.github.com>
2020-09-11 10:14:10 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,20 @@
+package incio.clase1;
+
+public class Usuario {
+
+	// atributos
+	// visibilidad + tipo + nombre
+	private String nombre;
+	private int loginFallidos;
+	protected int loginExitosos;
+
+	// métodos
+	// visibilidad + tipo de dato retonado + nomnre + (parámetros)
+	public String test() {
+
+		nombre = "ABC";
+		return nombre;
+
+	}
+
+}
